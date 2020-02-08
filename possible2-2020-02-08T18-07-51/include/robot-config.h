d1ef18401c41065f997eae2160a750c850428508
AcetheMace <59343035+AcetheMace@users.noreply.github.com>
2020-02-08 11:08:20 -0700

    first bit.

---
+++ possible2-2020-02-08T18-07-51/include/robot-config.h
@@ -0,0 +1,16 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern drivetrain Drivetrain;
+extern motor Lift;
+extern motor arm_lift_right;
+extern motor arm_lift_left;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
---
+++ possible2-2020-02-08T18-07-51/src/robot-config.cpp
@@ -0,0 +1,29 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor LeftDriveSmart = motor(PORT2, ratio18_1, false);
+motor RightDriveSmart = motor(PORT10, ratio18_1, true);
+drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 319.19, 295, 165, mm, 1);
+motor Lift = motor(PORT8, ratio18_1, false);
+motor arm_lift_right = motor(PORT9, ratio18_1, false);
+motor arm_lift_left = motor(PORT1, ratio18_1, true);
+
+// VEXcode generated functions
+
+
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
